2bc23f5f089f1836a67c2f702023ce36fe29adf7
harshavardhanp <harshavardhan.zodiac@gmail.com>
2020-03-03 15:33:46 +0530

    Completed /SHOP related API. Needs to be tested

--- ssn/src/main/java/com/food/ordering/ssn/dao/ItemDao.java
+++ ssn/src/main/java/com/food/ordering/ssn/dao/ItemDao.java
@@ -2,6 +2,8 @@ package com.food.ordering.ssn.dao;
 
 import java.util.List;
 
+import com.food.ordering.ssn.column.ItemColumn;
+import com.food.ordering.ssn.utils.ErrorLog;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
@@ -11,7 +13,6 @@ import org.springframework.stereotype.Repository;
 import com.food.ordering.ssn.model.ItemModel;
 import com.food.ordering.ssn.query.ItemQuery;
 import com.food.ordering.ssn.rowMapperLambda.ItemRowMapperLambda;
-import com.food.ordering.ssn.utils.Constant;
 import com.food.ordering.ssn.utils.Response;
 
 @Repository
@@ -23,72 +24,72 @@ public class ItemDao {
 	@Autowired
 	UtilsDao utilsDao;
 
-	public Response<List<ItemModel>> getItemsByShopId(Integer shopId, String oauthId) {
+	public Response<List<ItemModel>> getItemsByShopId(Integer shopId, String oauthId,String mobile) {
 		Response<List<ItemModel>> response = new Response<>();
 		List<ItemModel> list = null;
 
 		try {
 
-			if (utilsDao.validateUser(oauthId).getCode() != Constant.CodeSuccess)
+			if (utilsDao.validateUser(oauthId,mobile).getCode() != ErrorLog.CodeSuccess)
 				return response;
 
-			SqlParameterSource parameters = new MapSqlParameterSource().addValue("shop_id", shopId);
+			SqlParameterSource parameters = new MapSqlParameterSource().addValue(ItemColumn.shopId, shopId);
+			list = jdbcTemplate.query(ItemQuery.getItemsByShopId, parameters, ItemRowMapperLambda.itemRowMapperLambda);
 
-			list = jdbcTemplate.query(ItemQuery.getItemsByShopID, parameters, ItemRowMapperLambda.itemRowMapperLambda);
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
 			if (list != null && !list.isEmpty()) {
-				response.setCode(Constant.CodeSuccess);
-				response.setMessage(Constant.MessageSuccess);
+				response.setCode(ErrorLog.CodeSuccess);
+				response.setMessage(ErrorLog.Success);
 				response.setData(list);
 			}
 		}
 		return response;
 	}
 
-	public Response<ItemModel> getItemById(Integer itemId, String oauthIdRh) {
+	public Response<ItemModel> getItemById(Integer itemId, String oauthIdRh,String mobile) {
 		ItemModel item = null;
 		Response<ItemModel> response = new Response<>();
 
 		try {
 
-			if (utilsDao.validateUser(oauthIdRh).getCode() != Constant.CodeSuccess)
+			if (utilsDao.validateUser(oauthIdRh,mobile).getCode() != ErrorLog.CodeSuccess)
 				return response;
 
-			SqlParameterSource parameters = new MapSqlParameterSource().addValue("id", itemId);
-			item = jdbcTemplate.queryForObject(ItemQuery.getItemByID, parameters,
-					ItemRowMapperLambda.itemRowMapperLambda);
+			SqlParameterSource parameters = new MapSqlParameterSource().addValue(ItemColumn.id, itemId);
+			item = jdbcTemplate.queryForObject(ItemQuery.getItemById, parameters, ItemRowMapperLambda.itemRowMapperLambda);
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
 			if (item != null) {
-				response.setCode(Constant.CodeSuccess);
-				response.setMessage(Constant.MessageSuccess);
+				response.setCode(ErrorLog.CodeSuccess);
+				response.setMessage(ErrorLog.Success);
 				response.setData(item);
 			}
 		}
 		return response;
 	}
 
-	public Response<List<ItemModel>> getItemsByQuery(String oauthId, String query) {
+	public Response<List<ItemModel>> getItemsByName(Integer collegeId,String itemName,String oauthId,String mobile) {
 		Response<List<ItemModel>> response = new Response<>();
 		List<ItemModel> items = null;
 		try {
 
-			if (utilsDao.validateUser(oauthId).getCode() != Constant.CodeSuccess)
+			if (utilsDao.validateUser(oauthId,mobile).getCode() != ErrorLog.CodeSuccess)
 				return response;
 
-			SqlParameterSource parameters = new MapSqlParameterSource().addValue("query","%" + query + "%");
+			SqlParameterSource parameters = new MapSqlParameterSource().addValue(ItemColumn.name,itemName);
 
-			items = jdbcTemplate.query(ItemQuery.getItemsByQuery, parameters, ItemRowMapperLambda.itemRowMapperLambda);
+			items = jdbcTemplate.query(ItemQuery.getItemsByName, parameters, ItemRowMapperLambda.itemRowMapperLambda);
 
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
 			if (items != null && !items.isEmpty()) {
-				response.setCode(Constant.CodeSuccess);
-				response.setMessage(Constant.MessageSuccess);
+				response.setCode(ErrorLog.CodeSuccess);
+				response.setMessage(ErrorLog.Success);
 				response.setData(items);
 			}
 		}
--- ssn/src/main/java/com/food/ordering/ssn/dao/ShopDao.java
+++ ssn/src/main/java/com/food/ordering/ssn/dao/ShopDao.java
@@ -2,6 +2,12 @@ package com.food.ordering.ssn.dao;
 
 import java.util.List;
 
+import com.food.ordering.ssn.column.ShopColumn;
+import com.food.ordering.ssn.column.UserColumn;
+import com.food.ordering.ssn.model.ItemModel;
+import com.food.ordering.ssn.query.ItemQuery;
+import com.food.ordering.ssn.rowMapperLambda.ItemRowMapperLambda;
+import com.food.ordering.ssn.utils.ErrorLog;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
@@ -11,39 +17,67 @@ import org.springframework.stereotype.Repository;
 import com.food.ordering.ssn.model.ShopModel;
 import com.food.ordering.ssn.query.ShopQuery;
 import com.food.ordering.ssn.rowMapperLambda.ShopRowMapperLambda;
-import com.food.ordering.ssn.utils.Constant;
 import com.food.ordering.ssn.utils.Response;
 
 @Repository
 public class ShopDao {
 
 	@Autowired
-	NamedParameterJdbcTemplate jdbcTemplate;
+	NamedParameterJdbcTemplate namedParameterJdbcTemplate;
 	
 	@Autowired
 	UtilsDao utilsDao;
 	
-	public Response<List<ShopModel>> getShopsByCollegeId(Integer collegeId, String oauthId) {
+	public Response<List<ShopModel>> getShopsByCollegeId(Integer collegeId, String oauthId,String mobileNo) {
 		Response<List<ShopModel>> response = new Response<>();
 		List<ShopModel> list = null;
 
 		try {
 			
-			if(utilsDao.validateUser(oauthId).getCode() != Constant.CodeSuccess)
+			if(!utilsDao.validateUser(oauthId,mobileNo).getCode().equals(ErrorLog.CodeSuccess))
 				return response;
 			
-			SqlParameterSource parameters = new MapSqlParameterSource().addValue("college_id", collegeId);
+			SqlParameterSource parameters = new MapSqlParameterSource().addValue(ShopColumn.collegeId, collegeId);
+			list = namedParameterJdbcTemplate.query(ShopQuery.getShopByCollegeId, parameters, ShopRowMapperLambda.shopRowMapperLambda);
 
-			list = jdbcTemplate.query(ShopQuery.getShopsByCollegeID, parameters, ShopRowMapperLambda.shopRowMapperLambda);
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
 			if (list != null && !list.isEmpty()) {
-				response.setCode(Constant.CodeSuccess);
-				response.setMessage(Constant.MessageSuccess);
+				response.setCode(ErrorLog.CodeSuccess);
+				response.setMessage(ErrorLog.Success);
 				response.setData(list);
 			}
 		}
 		return response;
 	}
+
+	public Response<List<ItemModel>> getMenuByShopId(Integer shopId,String oauthId,String mobileNo){
+		Response<List<ItemModel>> response=new Response<>();
+		List<ItemModel> list=null;
+
+		try{
+
+			if(!utilsDao.validateUser(oauthId,mobileNo).getCode().equals(ErrorLog.CodeSuccess))
+				return response;
+
+			SqlParameterSource parameters=new MapSqlParameterSource().addValue(ShopColumn.id,shopId);
+			list=namedParameterJdbcTemplate.query(ItemQuery.getItemsByShopId,parameters, ItemRowMapperLambda.itemRowMapperLambda);
+
+		}catch(Exception e){
+			e.printStackTrace();
+		}finally {
+			if (list != null && !list.isEmpty()) {
+				response.setCode(ErrorLog.CodeSuccess);
+				response.setMessage(ErrorLog.Success);
+				response.setData(list);
+			}
+		}
+
+
+		return response;
+	}
+
+
+
 }
