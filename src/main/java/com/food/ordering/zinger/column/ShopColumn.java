3ea6a64579919333128a4fb88febf0413f34d373
Logesh Dinakaran <ddlogesh@gmail.com>
2020-03-10 08:59:04 +0530

    config apis added

--- src/main/java/com/food/ordering/zinger/column/ShopColumn.java
+++ src/main/java/com/food/ordering/zinger/column/ShopColumn.java
@@ -6,7 +6,7 @@ public class ShopColumn {
     public static final String id = "id";
     public static final String name = "name";
     public static final String photoUrl = "photo_url";
-    public static final String mobile = "photo_url";
+    public static final String mobile = "mobile";
     public static final String collegeId = "college_id";
     public static final String openingTime = "opening_time";
     public static final String closingTime = "closing_time";
