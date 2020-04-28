111cb21a99db2cf324088fd591b734cb5d12dfef
harshavardhanp <harshavardhan.zodiac@gmail.com>
2020-04-28 13:13:40 +0530

    Notification topic error solved

--- src/main/java/com/food/ordering/zinger/dao/impl/NotifyDaoImpl.java
+++ src/main/java/com/food/ordering/zinger/dao/impl/NotifyDaoImpl.java
@@ -218,7 +218,8 @@ public class NotifyDaoImpl implements NotifyDao {
             notificationModel.setPayload(jsonPayload);
 
             ShopModel shopModel = orderItemListModel.getTransactionModel().getOrderModel().getShopModel();
-            sendTopicMessage(notificationModel,shopModel.getName()+shopModel.getId());
+            String[] names = shopModel.getName().split(" ");
+            sendTopicMessage(notificationModel,names[0]+shopModel.getId());
 
         }
     }
