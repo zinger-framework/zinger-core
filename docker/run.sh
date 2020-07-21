c56740d5b59f7158b638af6ba11d92c0983c8898
Logesh Dinakaran <35095700+ddlogesh@users.noreply.github.com>
2020-07-21 21:20:11 +0530

    Integrated Docker (#1)
    * Integrated Docker

---
+++ docker/run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -vx
+
+bundle install
+
+bundle exec rake db:migrate
+
+rm -f tmp/pids/server.pid
+
+bundle exec rails s -p 80 -b '0.0.0.0' &
+bundle exec sidekiq 2>&1 >> log/sidekiq.log &
+
+tail -f log/sidekiq.log
